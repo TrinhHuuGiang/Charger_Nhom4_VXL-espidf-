146a19b696ffc828237dcecb8114f3e6676ec804
TrinhHuuGiang <giangtrinhvnbl2k2@gmail.com>
2024-01-09 10:29:27 +0700

    MCPfix

--- main/app_main.c
+++ main/app_main.c
@@ -3,6 +3,7 @@
 #include "freertos/task.h" //
 #include "driver/gpio.h"// driver gpio
 #include "sdkconfig.h" // cau hinh menuconfig
+#include "esp_log.h" // log thong bao
 #include "BTN.h"
 #include "Relay.h"
 #include "MCP41010.h"
@@ -21,13 +22,15 @@
 #define CS_PIN_1 4 // cs cho MCP41010 pin 1
 #define CS_PIN_2 2 // cs cho MCP41010 pin 2
 #define CS_PIN_3 15 // cs cho microsd cards
-#define MOSI 13// mosi (si) pin
-#define MISO 14 // miso (so) pin
-#define SCK // serial clock
+#define MOSI_PIN 13// mosi (si) pin
+#define MISO_PIN 12 // miso (so) pin
+#define SCK_PIN 14// serial clock
 
 // co 3 thiet bi dung i2c
 
 // co 2 thiet bi dung 1 wire
+
+#define TAG "MAIN"
 
 void app_main(void)
 {
@@ -38,9 +41,47 @@ void app_main(void)
     relay_init(RELAY_PIN_2,GPIO_MODE_OUTPUT,0);
     relay_init(RELAY_PIN_3,GPIO_MODE_OUTPUT,0);
 
-
+    MCP_t dev1;
+    mcp41010_init(&dev1, KOHMS_MCP41010, 2, MOSI_PIN, SCK_PIN,CS_PIN_1);
+    uint8_t cur_value1 = 0;
+    uint8_t DIRECTION1 = 1;
+    uint8_t potentiometer1 = METER_0;
+    MCP_t dev2;
+    mcp41010_init(&dev2, KOHMS_MCP41010, 2, MOSI_PIN, SCK_PIN,CS_PIN_2);
+    uint8_t cur_value2 = 0;
+    uint8_t DIRECTION2 = 1;
+    uint8_t potentiometer2 = METER_0;
 
     while (1)
+    {
+	MCP41_setWiper(&dev1, cur_value1, potentiometer1);
+	float resistance1 = MCP41_getK(&dev1, cur_value1);
+	ESP_LOGI(TAG, "cur_value=%d resistance=%f KOhms", cur_value1, resistance1);
+
+    if(DIRECTION1) {
+        cur_value1++;
+    if(cur_value1 == 255) DIRECTION1 = 0;
+    } else {
+        cur_value1--;
+    if(cur_value1 == 0) DIRECTION1 = 1;
+    }
+
+
+    MCP41_setWiper(&dev2, cur_value2, potentiometer2);
+	float resistance2 = MCP41_getK(&dev2, cur_value2);
+	ESP_LOGI(TAG, "cur_value=%d resistance=%f KOhms", cur_value2, resistance2);
+
+    if(DIRECTION2) {
+        cur_value2++;
+    if(cur_value2 == 255) DIRECTION2 = 0;
+    } else {
+        cur_value2--;
+    if(cur_value2 == 0) DIRECTION2 = 1;
+    }
+
+    vTaskDelay(50);
+
+    }
 
 #pragma region testrelay, button
     /* hàm test bấm button , relay bật tắt
